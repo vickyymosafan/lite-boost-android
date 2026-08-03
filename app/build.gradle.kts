3253b9f772f1db3548d716605a101641d7691ac0
vickyymosafan <mvickymosafan@gmail.com>
2026-08-03 18:25:25 +0700

    Restrict app to Android 9 and 10, enable Legacy Storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.optimizer.android"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 29
         versionCode = 1
         versionName = "1.0"
 
