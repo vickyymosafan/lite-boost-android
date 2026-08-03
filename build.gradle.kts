f0b7a698bfffbe02782e4af258c1fd577796b31d
vickyymosafan <mvickymosafan@gmail.com>
2026-08-03 17:21:57 +0700

    Fix syntax error in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 // Top-level build file
-buildscript {
-    ext {
-        compose_version = "1.5.0"
-    }
-}
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
