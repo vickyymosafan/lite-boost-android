1cca1ee00a95c9ce437a074bdcde2221c3eff1ed
vickyymosafan <mvickymosafan@gmail.com>
2026-09-11 13:49:12 +0700

    fix(build): bump AGP to 8.6.1 (newer R8 fixes ConcurrentModificationException on Kotlin 2.0 metadata)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
